6e2ff931f0ef9f8f0c28164942a1f967bb2fbb89
michaelyang12 <michaelyang@wustl.edu>
2021-11-26 20:03:30 -0600

    temp remove uploaduserimg

--- components/user_page/SingleUserPage.js
+++ components/user_page/SingleUserPage.js
@@ -43,12 +43,12 @@ function SingleUserPage(props) {
       <div className="-mt-12 flex flex-wrap w-2/3 h-auto overflow-auto border-4 border-black">
         {displayImages.map((image) => (
           <div className="w-96 min-h-0 max-h-72 object-fill mx-2 mt-2 border-black border-4">
-            <UploadedUserImages
+            {/* <UploadedUserImages
               user={props.user}
               isUserSelected={isUserSelected}
               image={image}
               email={email}
-            />
+            /> */}
           </div>
         ))}
       </div>
