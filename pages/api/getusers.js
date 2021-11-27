c1b2658c60fa0c301ac8e12018efee54aae1b347
justinneff5 <59897146+justinneff5@users.noreply.github.com>
2021-11-26 21:46:19 -0600

    get users comment cleanup

--- pages/api/getusers.js
+++ pages/api/getusers.js
@@ -5,15 +5,11 @@ const Lock = mongoose.model('Lock');
 
 export default async (req, res) => {
   const { method } = req;
-  //console.log('req');
-  //console.log(req);
   await connectDB(); //async connect to the database
   console.log('in getusers');
   if (method === 'POST') {
     try {
-      //const LC = req.body.lockCode;
       console.log('marker1');
-      //const sEmail = req.body.email;
       const data = {
         account_email: req.body.email,
       };
@@ -26,8 +22,6 @@ export default async (req, res) => {
         success: true,
         message: 'lock updated',
         statusText: 'user added',
-        //root_user: lock.parent_email,
-        //children_users: lock.children_emails,
         users: lock.users,
         images: lock.images,
       });
