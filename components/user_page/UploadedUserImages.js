a6f57d9b25872b170397b877737cc1a01ef911d7
justinneff5 <59897146+justinneff5@users.noreply.github.com>
2021-11-26 20:36:30 -0600

    uploaded images fixed

--- components/user_page/UploadedUserImages.js
+++ components/user_page/UploadedUserImages.js
@@ -6,15 +6,9 @@ function UploadedUserImages(props) {
   const image = props.image;
   const email = props.email;
   console.log(image);
-  const [bufferConv, setBufferConv] = useState('');
-  /*if (bufferConv === '') {
-    setBufferConv(btoa(String.fromCharCode.apply(null, image.img.data.data)));
-  }*/
-  var dataimg =
-    `data:${image.img.contentType};base64,` +
-    Buffer.from(String.fromCharCode.apply(null, image.img.data.data));
   function deleteImg(e) {
     e.preventDefault();
+    console.log('delete image call');
     axios
       .post('https://lockplus.tk/api/deletephoto', {
         email: email,
@@ -30,7 +24,12 @@ function UploadedUserImages(props) {
     <>
       <div className="h-full w-full relative border-blue">
         <div className="h-full w-full object-contain">
-          <img src={dataimg} alt="something went wrong" />
+          <img
+            src={`data:${image.img.contentType};base64,${Buffer.from(
+              image.img.data
+            ).toString('base64')}`}
+            alt="something went wrong"
+          />
         </div>
         <button
           className="absolute top-0 right-0 w-16 h-6 bg-red-500 rounded-bl-sm text-sm text-white font-lockplus focus:outline-none"
