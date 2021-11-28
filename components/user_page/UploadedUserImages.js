7bb9fcd11e1e98150b394225dc6144ce8d227444
justinneff5 <59897146+justinneff5@users.noreply.github.com>
2021-11-27 22:00:57 -0600

    converting preload to load upon single page...

--- components/user_page/UploadedUserImages.js
+++ components/user_page/UploadedUserImages.js
@@ -1,11 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function UploadedUserImages(props) {
   var visibility = props.isUserSelected ? 'visible' : 'invisible';
   const image = props.image;
+  //const [img, setImg] = useState({});
+  //const [cType, setCType] = useState('');
+  //const [buff, setBuff] = useState('');
+  const [source, setSource] = useState('');
+  const [apiDone, setApiDone] = useState(false);
   const email = props.email;
   console.log(image);
+  useEffect(() => {
+    console.log('is this even working?');
+    axios
+      .post(`/api/singlephoto/${image.filename}`, {
+        email: email,
+      })
+      .catch((err) => console.log(err))
+      .then((response) => {
+        console.log('response');
+        console.log(response.data);
+        setSource(
+          `data:${response.data.cType};base64,${Buffer.from(
+            response.data.buffer
+          ).toString('base64')}`
+        );
+        setApiDone(true);
+      });
+  }, []);
   function deleteImg(e) {
     e.preventDefault();
     console.log('delete image call');
@@ -22,21 +45,20 @@ function UploadedUserImages(props) {
   }
   return (
     <>
-      <div className="h-full w-full relative border-blue">
-        <div className="h-full w-full object-contain">
-          <img
-            src={`data:${image.img.contentType};base64,${Buffer.from(
-              image.img.data
-            ).toString('base64')}`}
-            alt="something went wrong"
-          />
+      {apiDone ? (
+        <div className="h-full w-full relative border-blue">
+          <div className="h-full w-full object-contain">
+            <img src={source} alt="something went wrong" />
+          </div>
+          <button
+            className="absolute top-0 right-0 w-16 h-6 bg-red-500 rounded-bl-sm text-sm text-white font-lockplus focus:outline-none"
+            onClick={deleteImg}>
+            delete
+          </button>
         </div>
-        <button
-          className="absolute top-0 right-0 w-16 h-6 bg-red-500 rounded-bl-sm text-sm text-white font-lockplus focus:outline-none"
-          onClick={deleteImg}>
-          delete
-        </button>
-      </div>
+      ) : (
+        <div className="h-full w-full relative border-blue"></div>
+      )}
     </>
   );
 }
--- pages/api/singlephoto/[filename].js
+++ pages/api/singlephoto/[filename].js
@@ -1,40 +1,41 @@
-import connectDB from '../../util/database';
-require('../../models/Lock');
+import connectDB from '../../../util/database';
+require('../../../models/Lock');
 import mongoose from 'mongoose';
 const Lock = mongoose.model('Lock');
 
 export default async (req, res) => {
   const { method } = req;
+  const filename = req.query.filename;
+  console.log(filename);
   await connectDB(); //async connect to the database
-  console.log('in getusers');
+  console.log('in single Photo');
   if (method === 'POST') {
     try {
       console.log('marker1');
       const data = {
         account_email: req.body.email,
       };
-      console.log('data');
-      console.log(data);
       const lock = await Lock.findOne(data);
-      console.log('lock');
-      console.log(lock);
-      let retImages = [];
-      lock.images.forEach((image) => {
-        retImages.push({ username: image.username, filename: image.filename });
+      let image;
+      lock.images.forEach((img) => {
+        if (img.filename === filename) {
+          image = img;
+        }
       });
+      console.log(image);
       res.status(201).json({
         success: true,
         message: 'lock updated',
         statusText: 'user added',
-        users: lock.users,
-        images: retImages,
+        buffer: image.img.data,
+        cType: image.img.contentType,
       });
     } catch (error) {
       console.log('error here');
       res.status(400).json({
         success: false,
         message: error.message,
-        statusText: 'error in add user',
+        statusText: 'error in singlephoto',
       });
     }
   } else {
