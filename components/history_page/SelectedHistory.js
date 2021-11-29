6afa9e8310e2a333529c8a8c8cff04547c2fc87d
justinneff5 <59897146+justinneff5@users.noreply.github.com>
2021-11-28 20:55:11 -0600

    fixing of space username

--- components/history_page/SelectedHistory.js
+++ components/history_page/SelectedHistory.js
@@ -1,7 +1,7 @@
 function SelectedHistory(props) {
   return (
     <>
-      <div className="h-full w-full bg-black text-white p-20">
+      <div className="h-full w-1/2 bg-black text-white p-20">
         {props.userIndex}
       </div>
     </>
