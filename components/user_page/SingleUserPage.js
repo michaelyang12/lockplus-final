477c031a6c43fccba2b2502e72150fdb1708328b
michaelyang12 <michaelyang@wustl.edu>
2021-11-28 19:01:41 -0600

    photo visibility test

--- components/user_page/SingleUserPage.js
+++ components/user_page/SingleUserPage.js
@@ -46,10 +46,10 @@ function SingleUserPage(props) {
         </div>
       </div>
       <div className="-mt-24 rounded-lg flex flex-wrap w-4/5 h-56 overflow-auto bg-lockplus-historyBlue">
-        <div class={`${visibility} absolute text-2xl font-regular left-96 mt-20 font-lockplus text-gray-700`}>
+        <div class={`invisible absolute text-2xl font-regular left-96 mt-20 font-lockplus text-gray-700`}>
           No Photos Found
         </div>
-        <div class={`${visibility} absolute text-lg font-regular left-72 mt-28 font-lockplus text-gray-700`}>
+        <div class={`invisible absolute text-lg font-regular left-72 mt-28 font-lockplus text-gray-700`}>
           Try uploading a photo using the prompt below
         </div>
         {displayImages.map((image) => (
